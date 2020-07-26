45399ca60723d1ff1a08e163e7dadf19df95d42c
begulatuk <65555163+begulatuk@users.noreply.github.com>
2020-07-26 21:55:55 +0700

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -59,7 +59,7 @@ if __name__ == "__main__" :
         bot_token=TG_BOT_TOKEN,
         api_id=APP_ID,
         api_hash=API_HASH,
-        workers=343
+        workers=444
     )
     #
     incoming_message_handler = MessageHandler(
